76fd9c85039eb29c1773444052c475273226beb6
walkloud <walksloud@gmail.com>
2025-09-29 12:47:48 -0700

    resolves #5 ; add bootstrap

--- make_example_group.sh
+++ make_example_group.sh
@@ -2,5 +2,7 @@
 
 npm install autoprefixer
 npm install postcss-import
+npm install bootstrap@5.3.8
+rsync -avu --progress --delete node_modules/bootstrap/ assets/sass/bootstrap/
 
 cp -r exampleSite/* .
